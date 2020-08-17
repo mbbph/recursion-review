f054ad21a17197ede88c606947064e78305e6e51
Lisa Hou <lqnhou@gmail.com>
2020-08-17 11:24:19 -0700

    implemented stringify and getelements and filled in the specs.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,29 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+//Solution implemented using closure recursion
+var getElementsHelper = function(e, n) { //e = element, n = target class name
+  var subResults = [];
+  //first, check if the current element has the class we're looking for.
+  var sublist = e.classList;
+  for (let i = 0; i < sublist.length; i++) {
+    if (sublist[i] === n) {
+      subResults.push(e); //add e to results
+      break;
+    }
+  }
+  //next, check if the element contains any children which have the class we're looking for.
+  var subChildren = e.childNodes; //recur on inner children
+  for (let i = 0; i < subChildren.length; i++) {
+    if (subChildren[i].nodeType === Node.ELEMENT_NODE) { //make sure we're only analyzing node elements
+      subResults = subResults.concat(getElementsHelper(subChildren[i], n)); //recur on children
+    }
+  }
+  return subResults;
+};
+
+//driver function that offloads the main process to the helper function.
+var getElementsByClassName = function(className) {
+  var b = document.body;
+  return getElementsHelper(b, className);
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,56 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
+//we're dealing with these types of objects:
+//array []
+//plain object {}
+//null
+//function
+//these objects can contain numbers, booleans, and strings
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //array
+  if (Array.isArray(obj)) { //if our object is an array
+    let result = '['; //start with '[' (and later close with ']')
+    for (let i = 0; i < obj.length; i++) { //iterate through array elements
+      let val = obj[i];
+      let string = stringifyJSON(val); //recur on each element and get the result
+      if (string === 'undefined') {
+        result = result + null + ','; //replace value with null, because deleting elements from arrays is not permitted and undefined is not a valid JSON value.
+      } else {
+        result = result + string + ',';
+      }
+    }
+    //we're always tacking on a comma after every array element, so we will end up with a comma before the closing bracket. We must delete it.
+    if (result.charAt(result.length - 1) === ',') {
+      result = result.slice(0, result.length - 1); //get rid of extra comma
+    }
+    return result + ']'; //add our closing bracket
+  //null
+  } else if (obj === null) { //null has type object so needs to be checked before plain object
+    return 'null';
+  //plain object
+  } else if (typeof obj === 'object') {
+    let result = '{';
+    for (const key in obj) { //iterate through (key, value) pairs
+      let val = obj[key];
+      let string = stringifyJSON(val); //recur on the value
+      if (string !== 'undefined') { //only add it to our resulting string if the element is not undefined.
+        result = result + '\"' + key + '\"' + ':' + string + ',';
+      }
+    }
+    //delete the extra comma
+    if (result.charAt(result.length - 1) === ',') {
+      result = result.slice(0, result.length - 1);
+    }
+    return result + '}';
+  //base cases
+  //Stringify turns functions and undefined values both into undefined.
+  } else if ((typeof obj === 'function') || (typeof obj === 'undefined')) {
+    return 'undefined';
+  } else if ((typeof obj === 'number') || (typeof obj === 'boolean')) { //number: ex: 9, true
+    return '' + obj;
+  } else { //(typeof obj === 'string')
+    return '\"' + obj + '\"';
+    //Note: There is the possibility that an element could be the string "undefined" rather than truly undefined. These two are differentiated in that strings get double quotes tacked onto both ends of them.
+  }
 };
